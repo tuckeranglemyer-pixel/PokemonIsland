688d1998a551c3081abf40e5f21f4dddb572a0e3
Tucker Anglemyer <tuckeranglemyer@gmail.com>
2026-03-13 13:06:54 -0500

    Initial Commit

---
+++ PokemonIsland/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PokemonIsland
+//
+//  Created by Tucker Anglemyer on 3/13/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PokemonIsland/PokemonIslandApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonIslandApp.swift
+//  PokemonIsland
+//
+//  Created by Tucker Anglemyer on 3/13/26.
+//
+
+import SwiftUI
+
+@main
+struct PokemonIslandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
